d592e42e91a3de56204f7d05258eeca414110664
stefan-zobel <stefan-zobel@localhost>
2014-12-27 00:39:58 +0100

    added DelegatingSpliterator

---
+++ src/main/java/java8/util/DelegatingSpliterator.java
@@ -0,0 +1,104 @@
+package java8.util;
+
+import java.util.Comparator;
+
+import java8.util.function.Consumer;
+import java8.util.function.Consumers;
+
+/**
+ * A j8.u.Spliterator implementation that delegates to a j.u.Spliterator.
+ *
+ * @param <T>
+ *            the type of the input to the operation
+ */
+final class DelegatingSpliterator<T> implements Spliterator<T> {
+
+	private final java.util.Spliterator<T> spliter;
+
+	DelegatingSpliterator(java.util.Spliterator<T> spliterator) {
+		Objects.requireNonNull(spliterator);
+		this.spliter = spliterator;
+	}
+
+	@Override
+	public boolean tryAdvance(Consumer<? super T> action) {
+		return spliter.tryAdvance(new ConsumerDelegate<>(action));
+	}
+
+	@Override
+	public Spliterator<T> trySplit() {
+		java.util.Spliterator<T> spliterator = spliter.trySplit();
+		if (spliterator == null) {
+			return null;
+		}
+		return new DelegatingSpliterator<>(spliterator);
+	}
+
+	@Override
+	public long estimateSize() {
+		return spliter.estimateSize();
+	}
+
+	@Override
+	public int characteristics() {
+		return spliter.characteristics();
+	}
+
+	@Override
+	public void forEachRemaining(Consumer<? super T> action) {
+		spliter.forEachRemaining(new ConsumerDelegate<>(action));
+	}
+
+	@Override
+	public long getExactSizeIfKnown() {
+		return spliter.getExactSizeIfKnown();
+	}
+
+	@Override
+	public boolean hasCharacteristics(int characteristics) {
+		return spliter.hasCharacteristics(characteristics);
+	}
+
+	@Override
+	public Comparator<? super T> getComparator() {
+		return spliter.getComparator();
+	}
+
+	/**
+	 * A j.u.f.Consumer implementation that delegates to a j8.u.f.Consumer.
+	 *
+	 * @param <T>
+	 *            the type of the input to the operation
+	 */
+	private static final class ConsumerDelegate<T> implements
+			java.util.function.Consumer<T> {
+
+		private final Consumer<T> delegate;
+
+		private ConsumerDelegate(Consumer<T> delegate) {
+			Objects.requireNonNull(delegate);
+			this.delegate = delegate;
+		}
+
+		@Override
+		public void accept(T t) {
+			delegate.accept(t);
+		}
+
+		// this can be spared in all likelihood !!?
+		@Override
+		public java.util.function.Consumer<T> andThen(
+				java.util.function.Consumer<? super T> after) {
+
+			Objects.requireNonNull(after);
+
+			return new ConsumerDelegate<T>(Consumers.andThen(delegate,
+					new java8.util.function.Consumer<T>() {
+						@Override
+						public void accept(T t) {
+							after.accept(t);
+						}
+					}));
+		}
+	}
+}
