45855aea93e9c7ce82f8369c4b0dd297f0feaced
stefan-zobel <stefan-zobel@localhost>
2015-05-07 20:51:19 +0200

    Add missing List.replaceAll method to j8.u.Lists (Ticket#61)

--- src/main/java/java8/util/Lists.java
+++ src/main/java/java8/util/Lists.java
@@ -27,6 +27,9 @@ package java8.util;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
+
+import java8.util.function.UnaryOperator;
 
 /**
  * A place for static default implementations of the new Java 8
@@ -35,10 +38,11 @@ import java.util.List;
  */
 public final class Lists {
     /**
-     * Sorts this list using the supplied {@code Comparator} to compare elements.
+     * Sorts the passed list using the supplied {@code Comparator} to compare
+     * elements.
      *
      * <p><b>Implementation Requirements:</b><br>
-     * The default implementation is equivalent to, for this {@code list}:
+     * The default implementation is equivalent to, for the passed {@code list}:
      * <pre>Collections.sort(list, c)</pre>
      *
      * @param <E> the type of the elements of the list to be sorted
@@ -50,14 +54,75 @@ public final class Lists {
      *         <i>mutually comparable</i> using the specified comparator
      * @throws UnsupportedOperationException if the list's list-iterator does
      *         not support the {@code set} operation
-     * @throws IllegalArgumentException
-     *         (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws IllegalArgumentException (optional)
      *         if the comparator is found to violate the {@link Comparator}
      *         contract
+     * @throws NullPointerException if the specified list is null
      * @since 1.8
      */
     public static <E> void sort(List<E> list, Comparator<? super E> c) {
         Collections.sort(list, c);
+    }
+
+    /**
+     * Replaces each element of the passed list with the result of applying the
+     * operator to that element.  Errors or runtime exceptions thrown by
+     * the operator are relayed to the caller.
+     *
+     * <p><b>Implementation Requirements:</b><br>
+     * The default implementation is equivalent to, for the passed {@code list}:
+     * <pre>{@code
+     *     final ListIterator<E> li = list.listIterator();
+     *     while (li.hasNext()) {
+     *         li.set(operator.apply(li.next()));
+     *     }
+     * }</pre>
+     *
+     * If the list's list-iterator does not support the {@code set} operation
+     * then an {@code UnsupportedOperationException} will be thrown when
+     * replacing the first element.
+     *
+     * @param <E> the type of the elements of the list to be replaced
+     * @param list the list whose elements should be replaced
+     * @param operator the operator to apply to each element
+     * @throws UnsupportedOperationException if the passed list is unmodifiable.
+     *         Implementations may throw this exception if an element
+     *         cannot be replaced or if, in general, modification is not
+     *         supported
+     * @throws NullPointerException if the specified list is null or the specified
+     *         operator is null or if the operator result is a null value and the
+     *         passed list does not permit null elements (optional)
+     * @since 1.8
+     */
+    public static <E> void replaceAll(List<E> list, UnaryOperator<E> operator) {
+        Objects.requireNonNull(list);
+        Objects.requireNonNull(operator);
+        ListIterator<E> li = list.listIterator();
+        while (li.hasNext()) {
+            li.set(operator.apply(li.next()));
+        }
+    }
+
+    /**
+     * Creates a {@link Spliterator} over the elements in the passed list.
+     *
+     * <p>The {@code Spliterator} reports at least {@link Spliterator#SIZED},
+     * {@link Spliterator#ORDERED} and {@link Spliterator#SUBSIZED}.
+     *
+     * <p><b>Implementation notes</b>:
+     * This implementation delegates to {@link Spliterators#spliterator(java.util.Collection)}
+     * so it is effectively the same as calling
+     * <pre>{@code
+     *     Spliterators.spliterator(list);
+     * }</pre> 
+     *
+     * @param <E> the type of the elements of the list to be splitted
+     * @param list the list to be splitted
+     * @return a {@code Spliterator} over the elements in the passed list
+     * @since 1.8
+     */
+    public static <E> Spliterator<E> spliterator(List<E> list) {
+        return Spliterators.spliterator(list);
     }
 
     private Lists() {
