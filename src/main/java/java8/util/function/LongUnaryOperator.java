c8285df42c034c78966b22579bd761205578e5c4
Stefan Zobel <spliterator@gmail.com>
2015-08-02 00:29:10 +0200

    Ticket#84: Clean up the interface code

--- src/main/java/java8/util/function/LongUnaryOperator.java
+++ src/main/java/java8/util/function/LongUnaryOperator.java
@@ -46,49 +46,4 @@ public interface LongUnaryOperator {
      * @return the operator result
      */
     long applyAsLong(long operand);
-
-    /**
-     * Returns a composed operator that first applies the {@code before}
-     * operator to its input, and then applies this operator to the result.
-     * If evaluation of either operator throws an exception, it is relayed to
-     * the caller of the composed operator.
-     *
-     * @param before the operator to apply before this operator is applied
-     * @return a composed operator that first applies the {@code before}
-     * operator and then applies this operator
-     * @throws NullPointerException if before is null
-     *
-     * @see #andThen(LongUnaryOperator)
-     */
-//    default LongUnaryOperator compose(LongUnaryOperator before) {
-//        Objects.requireNonNull(before);
-//        return (long v) -> applyAsLong(before.applyAsLong(v));
-//    }
-
-    /**
-     * Returns a composed operator that first applies this operator to
-     * its input, and then applies the {@code after} operator to the result.
-     * If evaluation of either operator throws an exception, it is relayed to
-     * the caller of the composed operator.
-     *
-     * @param after the operator to apply after this operator is applied
-     * @return a composed operator that first applies this operator and then
-     * applies the {@code after} operator
-     * @throws NullPointerException if after is null
-     *
-     * @see #compose(LongUnaryOperator)
-     */
-//    default LongUnaryOperator andThen(LongUnaryOperator after) {
-//        Objects.requireNonNull(after);
-//        return (long t) -> after.applyAsLong(applyAsLong(t));
-//    }
-
-    /**
-     * Returns a unary operator that always returns its input argument.
-     *
-     * @return a unary operator that always returns its input argument
-     */
-//    static LongUnaryOperator identity() {
-//        return t -> t;
-//    }
 }
