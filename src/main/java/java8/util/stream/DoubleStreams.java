e1261e56d0fc459b0a7f74d06182243795a1a3c9
stefan-zobel <stefan-zobel@localhost>
2015-08-05 00:26:00 +0200

    JDK-8132800 (Ticket#81) [Additional change in the Javadoc of Stream.concat]

--- src/main/java/java8/util/stream/DoubleStreams.java
+++ src/main/java/java8/util/stream/DoubleStreams.java
@@ -215,6 +215,8 @@ public final class DoubleStreams {
      * Use caution when constructing streams from repeated concatenation.
      * Accessing an element of a deeply concatenated stream can result in deep
      * call chains, or even {@code StackOverflowError}.
+     * <p>Subsequent changes to the sequential/parallel execution mode of the
+     * returned stream are not guaranteed to be propagated to the input streams.
      *
      * @param a the first stream
      * @param b the second stream
--- src/main/java/java8/util/stream/StreamSupport.java
+++ src/main/java/java8/util/stream/StreamSupport.java
@@ -175,6 +175,8 @@ public final class StreamSupport {
      * Use caution when constructing streams from repeated concatenation.
      * Accessing an element of a deeply concatenated stream can result in deep
      * call chains, or even {@code StackOverflowError}.
+     * <p>Subsequent changes to the sequential/parallel execution mode of the
+     * returned stream are not guaranteed to be propagated to the input streams.
      *
      * @param <T> The type of stream elements
      * @param a the first stream
