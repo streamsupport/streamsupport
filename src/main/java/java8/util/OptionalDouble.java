d7343988c605f57e4fe90066f2a833783d1ca73c
stefan-zobel <stefan-zobel@localhost>
2015-09-27 01:03:52 +0200

    fixed Javadoc typo

--- src/main/java/java8/util/OptionalDouble.java
+++ src/main/java/java8/util/OptionalDouble.java
@@ -80,7 +80,7 @@ public final class OptionalDouble {
      *
      * <p><b>API Note:</b><br> Though it may be tempting to do so, avoid testing if an object
      * is empty by comparing with {@code ==} against instances returned by
-     * {@code Option.empty()}. There is no guarantee that it is a singleton.
+     * {@code OptionalDouble.empty()}. There is no guarantee that it is a singleton.
      * Instead, use {@link #isPresent()}.
      *
      *  @return an empty {@code OptionalDouble}.
--- src/main/java/java8/util/OptionalInt.java
+++ src/main/java/java8/util/OptionalInt.java
@@ -91,7 +91,7 @@ public final class OptionalInt {
      *
      * <p><b>API Note:</b><br> Though it may be tempting to do so, avoid testing if an object
      * is empty by comparing with {@code ==} against instances returned by
-     * {@code Option.empty()}. There is no guarantee that it is a singleton.
+     * {@code OptionalInt.empty()}. There is no guarantee that it is a singleton.
      * Instead, use {@link #isPresent()}.
      *
      *  @return an empty {@code OptionalInt}
