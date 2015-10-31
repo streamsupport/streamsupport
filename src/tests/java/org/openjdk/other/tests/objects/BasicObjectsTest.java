d70bf7db700a708f85aeb77eb858a78755b71ab1
Stefan Zobel <spliterator@gmail.com>
2015-10-31 15:49:27 +0100

    revert JDK-8138963: new Objects method to default to non-null

--- src/tests/java/org/openjdk/other/tests/objects/BasicObjectsTest.java
+++ src/tests/java/org/openjdk/other/tests/objects/BasicObjectsTest.java
@@ -276,43 +276,43 @@ public class BasicObjectsTest {
 
     private static int testNonNullOf() {
         int errors = 0;
-        String defString = new String("default");
-        String nullString = null;
-        String nonNullString = "non-null";
-
-        // Confirm the compile time return type matches
-        String result = Objects.nonNullElse(nullString, defString);
-        errors += (result == defString) ? 0 : 1;
-        errors += (Objects.nonNullElse(nonNullString, defString) == nonNullString) ? 0 : 1;
-        errors += (Objects.nonNullElse(nonNullString, null) == nonNullString) ? 0 : 1;
-        try {
-            Objects.nonNullElse(null, null);
-            errors += 1;
-        } catch (NullPointerException npe) {
-            // expected
-            errors += npe.getMessage().equals("defaultObj") ? 0 : 1;
-        }
-
-
-        // Test nonNullElseGet with a supplier
-        errors += (Objects.nonNullElseGet(nullString, () -> defString) == defString) ? 0 : 1;
-        errors += (Objects.nonNullElseGet(nonNullString, () -> defString) == nonNullString) ? 0 : 1;
-        errors += (Objects.nonNullElseGet(nonNullString, () -> null) == nonNullString) ? 0 : 1;
-
-        try {
-            Objects.nonNullElseGet(null, () -> null);
-            errors += 1;
-        } catch (NullPointerException npe) {
-            // expected
-            errors += npe.getMessage().equals("supplier.get()") ? 0 : 1;
-        }
-        try {       // supplier is null
-            Objects.nonNullElseGet(null, null);
-            errors += 1;
-        } catch (NullPointerException npe) {
-            // expected
-            errors += npe.getMessage().equals("supplier") ? 0 : 1;
-        }
+//        String defString = new String("default");
+//        String nullString = null;
+//        String nonNullString = "non-null";
+//
+//        // Confirm the compile time return type matches
+//        String result = Objects.nonNullElse(nullString, defString);
+//        errors += (result == defString) ? 0 : 1;
+//        errors += (Objects.nonNullElse(nonNullString, defString) == nonNullString) ? 0 : 1;
+//        errors += (Objects.nonNullElse(nonNullString, null) == nonNullString) ? 0 : 1;
+//        try {
+//            Objects.nonNullElse(null, null);
+//            errors += 1;
+//        } catch (NullPointerException npe) {
+//            // expected
+//            errors += npe.getMessage().equals("defaultObj") ? 0 : 1;
+//        }
+//
+//
+//        // Test nonNullElseGet with a supplier
+//        errors += (Objects.nonNullElseGet(nullString, () -> defString) == defString) ? 0 : 1;
+//        errors += (Objects.nonNullElseGet(nonNullString, () -> defString) == nonNullString) ? 0 : 1;
+//        errors += (Objects.nonNullElseGet(nonNullString, () -> null) == nonNullString) ? 0 : 1;
+//
+//        try {
+//            Objects.nonNullElseGet(null, () -> null);
+//            errors += 1;
+//        } catch (NullPointerException npe) {
+//            // expected
+//            errors += npe.getMessage().equals("supplier.get()") ? 0 : 1;
+//        }
+//        try {       // supplier is null
+//            Objects.nonNullElseGet(null, null);
+//            errors += 1;
+//        } catch (NullPointerException npe) {
+//            // expected
+//            errors += npe.getMessage().equals("supplier") ? 0 : 1;
+//        }
         return errors;
     }
 }
