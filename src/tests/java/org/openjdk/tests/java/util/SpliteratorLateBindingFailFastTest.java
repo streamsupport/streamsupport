d8644643c9255d092b7fc9cb718a06cd16643546
stefan-zobel <stefan-zobel@localhost>
2016-03-04 21:42:25 +0100

    preliminaries for ticket #174

--- src/tests/java/org/openjdk/tests/java/util/SpliteratorLateBindingFailFastTest.java
+++ src/tests/java/org/openjdk/tests/java/util/SpliteratorLateBindingFailFastTest.java
@@ -70,7 +70,7 @@ public class SpliteratorLateBindingFailFastTest {
     }
 
     private static class SpliteratorDataBuilder<T> {
-        private static final boolean hasJDK8148748SublistBug = hasJDK8148748SublistBug();
+        private static final boolean hasJDK8148748SublistBug = JDK8148748SublistBugIndicator.BUG_IS_PRESENT;
 
         final List<Object[]> data;
 
@@ -213,29 +213,6 @@ public class SpliteratorLateBindingFailFastTest {
     }
 
     static Object[][] spliteratorDataProvider;
-
-    /**
-     * The Java 8/9 Spliterator for ArrayList.sublist() is currently not
-     * late-binding (https://bugs.openjdk.java.net/browse/JDK-8148748). We'd get
-     * test failures because of this when we run on Java 8/9 with Spliterator
-     * delegation enabled.
-     * 
-     * @return {@code true} on Java 8/9 when Spliterator delegation hasn't been
-     *         disabled, {@code false} otherwise.
-     */
-    private static boolean hasJDK8148748SublistBug() {
-        // a) must have at least major version number 52 (Java 8 or higher)
-        String ver = System.getProperty("java.class.version", "45");
-        if (ver != null && ver.length() >= 2) {
-            ver = ver.substring(0, 2);
-            if ("52".compareTo(ver) > 0) {
-                return false;
-            }
-        }
-        // b) Spliterator delegation must not be disabled
-        String s = System.getProperty(Spliterators.class.getName() + ".jre.delegation.enabled", Boolean.TRUE.toString());
-        return (s == null) || s.trim().equalsIgnoreCase(Boolean.TRUE.toString());
-    }
 
     @DataProvider(name = "Source")
     public static Object[][] spliteratorDataProvider() {
