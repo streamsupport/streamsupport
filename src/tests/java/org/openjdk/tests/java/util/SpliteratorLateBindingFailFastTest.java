fe9877bb78f556c475993a699574f8257ec7cadf
Stefan Zobel <spliterator@gmail.com>
2016-03-07 21:49:28 +0100

    Prepare for introduction of a RandomAccess / AbstractList based Spliterator

--- src/tests/java/org/openjdk/tests/java/util/SpliteratorLateBindingFailFastTest.java
+++ src/tests/java/org/openjdk/tests/java/util/SpliteratorLateBindingFailFastTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -225,6 +225,8 @@ public class SpliteratorLateBindingFailFastTest {
 
         // Collections
 
+        db.addList(RandomAccessList::new);
+
         db.addList(ArrayList::new);
 
         db.addList(LinkedList::new);
@@ -396,5 +398,4 @@ public class SpliteratorLateBindingFailFastTest {
                    String.format("Exception thrown %s not an instance of %s",
                                  caught.getClass().getName(), expected.getName()));
     }
-
 }
