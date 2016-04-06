a5e2d47b5c254b76a644602552f877ea87695839
stefan-zobel <stefan-zobel@localhost>
2016-04-06 22:46:22 +0200

    Add List.subList() test(s) to Java 9 tests (#189)

--- src/tests/java/org/openjdk/tests/java/util/JDK8148748SublistBugIndicator.java
+++ src/tests/java/org/openjdk/tests/java/util/JDK8148748SublistBugIndicator.java
@@ -7,26 +7,31 @@ final class JDK8148748SublistBugIndicator {
     static final boolean BUG_IS_PRESENT = hasJDK8148748SublistBug();
 
     /**
-     * The Java 8/9 Spliterator for ArrayList.sublist() is currently not
+     * The Java 8 Spliterator for ArrayList.sublist() is currently not
      * late-binding (https://bugs.openjdk.java.net/browse/JDK-8148748). We'd get
-     * test failures because of this when we run on Java 8/9 with Spliterator
-     * delegation enabled.
+     * test failures because of this when we run on Java 8 with Spliterator
+     * delegation enabled. This bug has been fixed in Java 9 ea build 112, so we
+     * require this as the minimum version for test runs on Java 9 (otherwise
+     * we'd also get failures on Java 9 since we do not test for class.version
+     * 53.0 here).
      * 
-     * @return {@code true} on Java 8/9 when Spliterator delegation hasn't been
+     * @return {@code true} on Java 8 when Spliterator delegation hasn't been
      *         disabled, {@code false} otherwise.
      */
     private static boolean hasJDK8148748SublistBug() {
-        // a) must have at least major version number 52 (Java 8 or higher)
+        // a) must have exactly major version number 52 (Java 8)
         String ver = System.getProperty("java.class.version", "45");
         if (ver != null && ver.length() >= 2) {
             ver = ver.substring(0, 2);
-            if ("52".compareTo(ver) > 0) {
-                return false;
+            if ("52".equals(ver)) {
+                // b) Spliterator delegation must not be disabled
+                String s = System.getProperty(Spliterators.class.getName()
+                        + ".jre.delegation.enabled", Boolean.TRUE.toString());
+                return (s == null)
+                        || s.trim().equalsIgnoreCase(Boolean.TRUE.toString());
             }
         }
-        // b) Spliterator delegation must not be disabled
-        String s = System.getProperty(Spliterators.class.getName() + ".jre.delegation.enabled", Boolean.TRUE.toString());
-        return (s == null) || s.trim().equalsIgnoreCase(Boolean.TRUE.toString());
+        return false;
     }
 
     private JDK8148748SublistBugIndicator() {
