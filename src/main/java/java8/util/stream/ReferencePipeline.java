e479a39877634028aacbb4c2b148aa1b2f2c9eec
Stefan Zobel <spliterator@gmail.com>
2017-01-22 00:22:27 +0100

    Prepare for #266 (eliminate SinkConsumer compiler kludge)

--- src/main/java/java8/util/stream/ReferencePipeline.java
+++ src/main/java/java8/util/stream/ReferencePipeline.java
@@ -476,7 +476,7 @@ abstract class ReferencePipeline<P_IN, P_OUT>
         // super type of U an ArrayStoreException will be thrown.
         @SuppressWarnings("rawtypes")
         IntFunction rawGenerator = (IntFunction) generator;
-        return (A[]) Nodes.flatten(evaluateToArrayNode(rawGenerator), rawGenerator)
+        return (A[]) Nodes.flatten((Node<A>) evaluateToArrayNode(rawGenerator), generator)
                               .asArray(rawGenerator);
     }
 
