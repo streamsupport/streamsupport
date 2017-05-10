2d2203cdc91cf22c514e14b3616272f1e8ae277f
Stefan Zobel <spliterator@gmail.com>
2017-05-10 20:57:45 +0200

    TLRandom: revert static initializer changes from commit [3e381f] (#301)

--- src/main/java/java8/util/concurrent/TLRandom.java
+++ src/main/java/java8/util/concurrent/TLRandom.java
@@ -209,6 +209,23 @@ import java.util.concurrent.atomic.AtomicLong;
         }
     }
 
+    /**
+     * Are we running on a pre-Java8 IBM VM?
+     * @return
+     */
+    private static boolean isIBMPre8() {
+        if (isClassPresent("com.ibm.misc.JarVersion")) {
+            String ver = System.getProperty("java.class.version", "45");
+            if (ver != null && ver.length() >= 2) {
+                ver = ver.substring(0, 2);
+                if ("52".compareTo(ver) > 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     private static boolean isClassPresent(String name) {
         Class<?> clazz = null;
         try {
@@ -224,6 +241,7 @@ import java.util.concurrent.atomic.AtomicLong;
     // Unsafe mechanics
     private static final sun.misc.Unsafe U = UnsafeAccess.unsafe;
     private static final long VALUE_OFF;
+    private static final boolean IS_PRE8_IBM;
     private static final boolean IS_ANDROID;
     private static final long THREADLOCALS;
     private static final long INHERITABLETHREADLOCALS;
@@ -231,15 +249,18 @@ import java.util.concurrent.atomic.AtomicLong;
     private static final long CCL;
     static {
         try {
+            IS_PRE8_IBM = isIBMPre8();
             IS_ANDROID = isAndroid();
             if (!IS_ANDROID) {
                 THREADLOCALS = U.objectFieldOffset(Thread.class
                         .getDeclaredField("threadLocals"));
                 INHERITABLETHREADLOCALS = U.objectFieldOffset(Thread.class
                         .getDeclaredField("inheritableThreadLocals"));
+                String accFieldName = IS_PRE8_IBM ? "accessControlContext"
+                        : "inheritedAccessControlContext";
                 INHERITEDACCESSCONTROLCONTEXT = U
                         .objectFieldOffset(Thread.class
-                                .getDeclaredField("inheritedAccessControlContext"));
+                                .getDeclaredField(accFieldName));
             } else {
                 // we don't need these offsets when on Android
                 THREADLOCALS = 0L;
