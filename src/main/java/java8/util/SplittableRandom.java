1375b17f4760d9322f68512e573fa1905a6dca4b
Stefan Zobel <spliterator@gmail.com>
2017-10-13 21:53:43 +0200

    whitespace

--- src/main/java/java8/util/SplittableRandom.java
+++ src/main/java/java8/util/SplittableRandom.java
@@ -415,7 +415,7 @@ public final class SplittableRandom {
     public void nextBytes(byte[] bytes) {
         int i = 0;
         int len = bytes.length;
-        for (int words = len >> 3; words--> 0; ) {
+        for (int words = len >> 3; words-- > 0; ) {
             long rnd = nextLong();
             for (int n = 8; n--> 0; rnd >>>= Byte.SIZE)
                 bytes[i++] = (byte) rnd;
